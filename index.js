f34d65d373e9e6828510f3f30785b87e2572f4ec
rajatrjsharma <merajatsharma@outlook.com>
2024-03-06 22:11:36 +0530

    Small Update

--- index.js
+++ index.js
@@ -7,23 +7,29 @@ let loading = false;
 if (button)
   button.addEventListener("click", async () => {
     if (!loading) {
-      const url = "https://api.quotable.io/random";
-      const options = {
-        method: "GET",
-      };
-      loading = true;
-      if (button) button.classList.toggle("button--loading");
-      try {
-        const quoteResponse = await fetch(url, options);
-        const quoteResult = await quoteResponse.json();
-        if (body) body.innerText = '"' + quoteResult.content + '"';
-        loading = false;
-        if (button) button.classList.toggle("button--loading");
-      } catch (error) {
-        console.error(error);
-        if (body) body.innerText = "Sorry API error !!";
-        loading = false;
-        if (button) button.classList.toggle("button--loading");
-      }
+      getQuote();
     }
   });
+
+const getQuote = async () => {
+  const url = "https://api.quotable.io/random";
+  const options = {
+    method: "GET",
+  };
+  loading = true;
+  if (button) button.classList.toggle("button--loading");
+  try {
+    const quoteResponse = await fetch(url, options);
+    const quoteResult = await quoteResponse.json();
+    if (body) body.innerText = '"' + quoteResult.content + '"';
+    loading = false;
+    if (button) button.classList.toggle("button--loading");
+  } catch (error) {
+    console.error(error);
+    if (body) body.innerText = "Sorry API error !!";
+    loading = false;
+    if (button) button.classList.toggle("button--loading");
+  }
+};
+
+getQuote();
