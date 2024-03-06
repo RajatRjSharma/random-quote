e806a4a473b9a51ce399cf4d3a8ec3e47fb69973
rajatrjsharma <merajatsharma@outlook.com>
2024-03-06 22:08:21 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,29 @@
+const button = document.getElementById("button");
+
+const body = document.getElementById("body");
+
+let loading = false;
+
+if (button)
+  button.addEventListener("click", async () => {
+    if (!loading) {
+      const url = "https://api.quotable.io/random";
+      const options = {
+        method: "GET",
+      };
+      loading = true;
+      if (button) button.classList.toggle("button--loading");
+      try {
+        const quoteResponse = await fetch(url, options);
+        const quoteResult = await quoteResponse.json();
+        if (body) body.innerText = '"' + quoteResult.content + '"';
+        loading = false;
+        if (button) button.classList.toggle("button--loading");
+      } catch (error) {
+        console.error(error);
+        if (body) body.innerText = "Sorry API error !!";
+        loading = false;
+        if (button) button.classList.toggle("button--loading");
+      }
+    }
+  });
